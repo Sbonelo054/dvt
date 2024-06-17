27f3e011e4d1e958cd3aadc03eea00c4cc276491
Sbonelo054 <sbonelongqondo54@gmail.com>
2024-06-17 11:16:35 +0200

    Added UI Components Renamed Models

---
+++ app/src/main/java/com/dvt/dvtapp/model/forecast/ForecastWeather.kt
@@ -0,0 +1,10 @@
+package com.dvt.dvtapp.model.forecast
+
+import com.google.gson.annotations.SerializedName
+
+data class ForecastWeather(@SerializedName("cod"     ) var cod     : String?         = null,
+                           @SerializedName("message" ) var message : Int?            = null,
+                           @SerializedName("cnt"     ) var cnt     : Int?            = null,
+                           @SerializedName("list"    ) var weatherList    : ArrayList<WeatherList> = arrayListOf(),
+                           @SerializedName("city"    ) var city    : City?           = City()
+)
--- app/src/main/java/com/dvt/dvtapp/networking/API.kt
+++ app/src/main/java/com/dvt/dvtapp/networking/API.kt
@@ -1,13 +1,13 @@
 package com.dvt.dvtapp.networking
 
 import com.dvt.dvtapp.model.current.CurrentWeather
-import com.dvt.dvtapp.model.focast.FocastWeather
+import com.dvt.dvtapp.model.forecast.ForecastWeather
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface API {
     @GET("forecast")
-    suspend fun getForecast(@Query("q") place: String?, @Query("units") units: String, @Query("appid") key: String): FocastWeather
+    suspend fun getForecast(@Query("q") place: String?, @Query("units") units: String, @Query("appid") key: String): ForecastWeather
 
     @GET("weather")
     suspend fun getCurrentWeather(@Query("q") place: String?, @Query("appid") key: String): CurrentWeather
