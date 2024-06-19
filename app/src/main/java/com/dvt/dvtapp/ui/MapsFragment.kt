24ded0388cd61cb7a208b3e4527e9ff8ecb9e694
Sbonelo054 <sbonelongqondo54@gmail.com>
2024-06-19 16:35:32 +0200

    Fixed Colors

--- app/src/main/java/com/dvt/dvtapp/ui/MapsFragment.kt
+++ app/src/main/java/com/dvt/dvtapp/ui/MapsFragment.kt
@@ -77,16 +77,6 @@ class MapsFragment : Fragment() {
         }
         val task = fusedLocationClient?.lastLocation
         task?.addOnSuccessListener { location ->
-            supportMapFragment?.getMapAsync { googleMap ->
-                if (location != null) {
-                    favouriteViewModel.getFavourites()?.observe(viewLifecycleOwner) {
-                        if (it != null) {
-                            for (place in it) {
-                                val geocoder = Geocoder(requireContext(), Locale.getDefault())
-                                val addresses: List<Address>? =
-                                    geocoder.getFromLocationName(place.place, 1)
-                                val lat: Double? = addresses?.get(0)?.latitude
-                                val lon: Double? = addresses?.get(0)?.longitude
             supportMapFragment?.getMapAsync(object : OnMapReadyCallback {
                 override fun onMapReady(googleMap: GoogleMap) {
                     if (location != null) {
@@ -94,33 +84,35 @@ class MapsFragment : Fragment() {
                             if (it != null) {
                                 for (place in it) {
                                     val geocoder = Geocoder(requireContext(), Locale.getDefault())
-                                    val addresses: List<Address>? = geocoder.getFromLocationName(place.place, 1)
+                                    val addresses: List<Address>? =
+                                        geocoder.getFromLocationName(place.place, 10)
                                     val lat: Double? = addresses?.get(0)?.latitude
                                     val lon: Double? = addresses?.get(0)?.longitude
 
-                                if (lat != null && lon != null) {
-                                    val favLatLon = LatLng(lat, lon)
-                                    val locationArrayList: ArrayList<LatLng> = ArrayList()
-                                    locationArrayList.add(favLatLon)
-                                    for (i in locationArrayList.indices) {
-                                        googleMap.addMarker(
-                                            MarkerOptions().position(
-                                                locationArrayList[i]
-                                            ).title(getString(R.string.favourites))
-                                        )
-                                        googleMap.animateCamera(CameraUpdateFactory.zoomTo(18.0f))
-                                        googleMap.moveCamera(
-                                            CameraUpdateFactory.newLatLng(
-                                                locationArrayList[i]
+                                    if (lat != null && lon != null) {
+                                        val favLatLon = LatLng(lat, lon)
+                                        val locationArrayList: ArrayList<LatLng> = ArrayList()
+                                        locationArrayList.add(favLatLon)
+                                        for (i in locationArrayList.indices) {
+                                            googleMap.addMarker(
+                                                MarkerOptions().position(
+                                                    locationArrayList[i]
+                                                ).title("Favourites")
                                             )
-                                        )
+                                            googleMap.animateCamera(CameraUpdateFactory.zoomTo(18.0f))
+                                            googleMap.moveCamera(
+                                                CameraUpdateFactory.newLatLng(
+                                                    locationArrayList[i]
+                                                )
+                                            )
+                                        }
                                     }
                                 }
                             }
                         }
                     }
                 }
-            }
+            })
         }
     }
 }
